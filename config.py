4933b885a1c819a3302c5cb6969e13f65c5fe057
JanieTran <emerald1901@gmail.com>
2020-09-12 21:26:42 +0700

    Notebooks on audio properties and features

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 MEMBER_TO_LABEL = {
     'Jin': 0, 'Suga': 1, 'J-Hope': 2, 'RM': 3, 'Jimin': 4, 'V': 5, 'Jungkook': 6
 }
-LABEL_TO_MEMBER = {label:member for member, label in MEMBER_TO_LABEL.items()}
+LABEL_TO_MEMBER = {label: member for member, label in MEMBER_TO_LABEL.items()}
 
 MAX_LEN_MS = 5000
 MIN_LEN_MS = 2000
+
+NUM_LABELS = len(MEMBER_TO_LABEL)
